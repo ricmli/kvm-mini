08dfba93b54cb8721c5bdef900ae988fa20514a0
ricmli <ricmli@outlook.com>
2025-11-12 10:34:05 +0800

    create vm and vcpu

--- src/main.zig
+++ src/main.zig
@@ -2,13 +2,24 @@ const std = @import("std");
 const kvm_mini = @import("kvm_mini");
 
 pub fn main() !void {
-    const v = try kvm_mini.getKvmVersion();
-    std.debug.print("KVM API version: {d}\n", .{v});
+    const allocator = &std.heap.page_allocator;
 
-    const hasExtension = try kvm_mini.checkExtension(kvm_mini.kvm.KVM_CAP_USER_MEMORY);
-    if (hasExtension) {
-        std.debug.print("KVM_CAP_USER_MEMORY is supported\n", .{});
-    } else {
-        std.debug.print("KVM_CAP_USER_MEMORY is NOT supported\n", .{});
-    }
+    var k = try kvm_mini.Kvm.open(allocator);
+    defer k.close() catch {};
+
+    const v = try k.getVersion();
+    std.debug.print("KVM API version from Kvm struct: {d}\n", .{v});
+
+    const hasExtension = try k.checkExtension(kvm_mini.kvm.KVM_CAP_USER_MEMORY);
+    std.debug.print("KVM_CAP_USER_MEMORY supported: {s}\n", .{if (hasExtension) "yes" else "no"});
+
+    var vm = try k.createVm();
+    defer vm.close() catch {};
+
+    std.debug.print("Created KVM fd: {d} VM fd: {d}\n", .{ k.kvm_fd, vm.vm_fd });
+
+    const vcpuFd = try vm.createVcpu(0);
+    std.debug.print("Created VCPU with fd: {d}\n", .{vcpuFd});
+
+    // vm.close() will close vcpus and vm fds; k.close() will close the /dev/kvm fd
 }
--- src/root.zig
+++ src/root.zig
@@ -4,38 +4,95 @@ const os = std.os.linux;
 const ioctl = std.os.linux.ioctl;
 pub const kvm = @import("generated_kvm.zig");
 
-// Only provide read-only functions for now.
+pub const Error = error{
+    OpenKvmFailed,
+    IoctlFailed,
+    CloseVmFailed,
+    CloseVcpuFailed,
+};
 
-pub fn getKvmVersion() !u32 {
-    const fd = os.open("/dev/kvm", .{ .ACCMODE = .RDONLY }, 0);
-    if (@as(isize, @bitCast(fd)) < 0) {
-        return error.OpenKvmFailed;
-    }
-    defer _ = os.close(@intCast(fd));
+pub const Vm = struct {
+    vm_fd: i32,
+    vcpus: [16]u32,
+    vcpu_count: usize,
 
-    const ret = ioctl(@intCast(fd), kvm.KVM_GET_API_VERSION, 0);
-    if (@as(isize, @bitCast(ret)) < 0) {
-        std.debug.print("ioctl failed with return code: {d}\n", .{
-            @as(isize, @bitCast(ret)),
-        });
-        return error.IoctlFailed;
+    pub fn createVcpu(self: *Vm, vcpuId: u32) !u32 {
+        const ret = ioctl(@intCast(self.vm_fd), kvm.KVM_CREATE_VCPU, @intCast(vcpuId));
+        if (@as(isize, @bitCast(ret)) < 0) {
+            return Error.IoctlFailed;
+        }
+        const fd: u32 = @intCast(ret);
+        if (self.vcpu_count >= self.vcpus.len) {
+            // simple fixed-capacity implementation: refuse to add more
+            return Error.IoctlFailed;
+        }
+        self.vcpus[self.vcpu_count] = fd;
+        self.vcpu_count += 1;
+        return fd;
     }
-    return @intCast(ret);
-}
 
-pub fn checkExtension(extension: u32) !bool {
-    const fd = os.open("/dev/kvm", .{ .ACCMODE = .RDONLY }, 0);
-    if (@as(isize, @bitCast(fd)) < 0) {
-        return error.OpenKvmFailed;
-    }
-    defer _ = os.close(@intCast(fd));
+    pub fn close(self: *Vm) !void {
+        // close vcpus
+        for (self.vcpus[0..self.vcpu_count]) |fd| {
+            _ = os.close(@intCast(fd));
+        }
 
-    const ret = ioctl(@intCast(fd), kvm.KVM_CHECK_EXTENSION, @intCast(extension));
-    if (@as(isize, @bitCast(ret)) < 0) {
-        std.debug.print("ioctl failed with return code: {d}\n", .{
-            @as(isize, @bitCast(ret)),
-        });
-        return error.IoctlFailed;
+        if (self.vm_fd != -1) {
+            const r = os.close(@intCast(self.vm_fd));
+            if (r != 0) return Error.CloseVmFailed;
+            self.vm_fd = -1;
+        }
     }
-    return ret != 0;
-}
+};
+
+pub const Kvm = struct {
+    allocator: *const std.mem.Allocator,
+    kvm_fd: i32,
+
+    pub fn open(allocator: *const std.mem.Allocator) !Kvm {
+        const kfd = os.open("/dev/kvm", .{ .ACCMODE = .RDONLY }, 0);
+        if (@as(isize, @bitCast(kfd)) < 0) {
+            return Error.OpenKvmFailed;
+        }
+        return Kvm{
+            .allocator = allocator,
+            .kvm_fd = @intCast(kfd),
+        };
+    }
+
+    pub fn createVm(self: *Kvm) !Vm {
+        const ret = ioctl(@intCast(self.kvm_fd), kvm.KVM_CREATE_VM, 0);
+        if (@as(isize, @bitCast(ret)) < 0) {
+            return Error.IoctlFailed;
+        }
+        return Vm{
+            .vm_fd = @intCast(ret),
+            .vcpus = [_]u32{0} ** 16,
+            .vcpu_count = 0,
+        };
+    }
+
+    pub fn getVersion(self: *Kvm) !u32 {
+        const ret = ioctl(@intCast(self.kvm_fd), kvm.KVM_GET_API_VERSION, 0);
+        if (@as(isize, @bitCast(ret)) < 0) {
+            return Error.IoctlFailed;
+        }
+        return @intCast(ret);
+    }
+
+    pub fn checkExtension(self: *Kvm, extension: u32) !bool {
+        const ret = ioctl(@intCast(self.kvm_fd), kvm.KVM_CHECK_EXTENSION, @intCast(extension));
+        if (@as(isize, @bitCast(ret)) < 0) {
+            return Error.IoctlFailed;
+        }
+        return ret != 0;
+    }
+
+    pub fn close(self: *Kvm) !void {
+        if (self.kvm_fd != -1) {
+            const r = os.close(@intCast(self.kvm_fd));
+            if (r != 0) return Error.CloseVmFailed;
+            self.kvm_fd = -1;
+        }
+    }
+};
